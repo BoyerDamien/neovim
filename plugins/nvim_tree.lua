0e685199930de63c2613904d6d805c951f5ba889
BOYER_Damien <damien.boyer@astrachain.com>
2022-06-04 09:36:22 +0200

    fix: nvim tree update

--- plugins/nvim_tree.lua
+++ plugins/nvim_tree.lua
@@ -5,7 +5,7 @@ vim.g.nvim_tree_root_folder_modifier = ":~" --This is the default. See :help fil
 
 vim.g.nvim_tree_width_allow_resize = 0 --0 by default, will not resize the tree when opening a file
 
-vim.g.nvim_tree_add_trailing = 1 --0 by default, append a trailing slash to folder names
+-- vim.g.nvim_tree_add_trailing = 1 --0 by default, append a trailing slash to folder names
 
 vim.g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
 
