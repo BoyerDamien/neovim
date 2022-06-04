a528b47a6fdc90b2f1d0394b80d7ad5112485a46
BOYER_Damien <damien.boyer@astrachain.com>
2022-06-04 09:37:45 +0200

    fix: remove useless lines

--- plugins/init.lua
+++ plugins/init.lua
@@ -12,7 +12,6 @@ require("packer").startup(function()
                 "kyazdani42/nvim-web-devicons", -- optional, for file icon
             },
             config = function()
-                -- require("plugins/nvim_tree")
                 -- setup with all defaults
                 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
                 -- nested options are documented by accessing them with `.` (eg: `:help nvim-tree.view.mappings.list`).
