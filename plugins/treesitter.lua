15f2751bfb7dc2300882d9ba7acbb850d4b42a27
BOYER_Damien <damien.boyer@astrachain.com>
2022-07-10 17:41:43 +0200

    fix: select langage parser for treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,10 +1,24 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = "all",
+    ensure_installed = {
+        "go",
+        "cpp",
+        "c",
+        "rust",
+        "json",
+        "css",
+        "dockerfile",
+        "html",
+        "markdown",
+        "python",
+        "javascript",
+        "typescript",
+        "yaml"
+    },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
     autopairs = { enable = true },
     -- List of parsers to ignore installing
-    ignore_install = { "ocamllex", "teal", "devicetree", "godot_resource" },
+    ignore_install = { "ocamllex", "teal", "devicetree", "godot_resource", "php" },
 })
