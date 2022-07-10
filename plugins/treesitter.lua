a17f04a8b3e9d1f20d7589ab230e37c9555aa832
BOYER_Damien <damien.boyer@astrachain.com>
2022-07-10 17:49:53 +0200

    fix: add all langage parsers + filter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,26 +1,10 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-        "go",
-        "gomod",
-        "comment",
-        "cpp",
-        "c",
-        "rust",
-        "json",
-        "css",
-        "dockerfile",
-        "html",
-        "markdown",
-        "python",
-        "javascript",
-        "typescript",
-        "yaml"
-    },
+    ensure_installed = "all",
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
     autopairs = { enable = true },
     -- List of parsers to ignore installing
-    ignore_install = { "ocamllex", "teal", "devicetree", "godot_resource", "php" },
+    ignore_install = { "ocamllex", "teal", "devicetree", "godot_resource", "php", "phpdoc" },
 })
