eac0e013c93cf3e53cba54231e13347732f3b87d
BOYER_Damien <damien.boyer@astrachain.com>
2023-04-12 10:29:19 +0200

    feat: add test/ide integration

--- plugins/debug/init.lua
+++ plugins/debug/init.lua
@@ -1,108 +1,107 @@
 local m = {}
 
 local ui_cfg = {
-	controls = {
-		element = "repl",
-		enabled = true,
-		icons = {
-			disconnect = "",
-			pause = "",
-			play = "",
-			run_last = "",
-			step_back = "",
-			step_into = "",
-			step_out = "",
-			step_over = "",
-			terminate = "",
-		},
-	},
-	element_mappings = {},
-	expand_lines = true,
-	floating = {
-		border = "single",
-		mappings = {
-			close = { "q", "<Esc>" },
-		},
-	},
-	force_buffers = true,
-	icons = {
-		collapsed = "",
-		current_frame = "",
-		expanded = "",
-	},
-	layouts = {
-		{
-			elements = {
-				{
-					id = "scopes",
-					size = 0.25,
-				},
-				{
-					id = "breakpoints",
-					size = 0.25,
-				},
-				{
-					id = "stacks",
-					size = 0.25,
-				},
-				{
-					id = "watches",
-					size = 0.25,
-				},
-			},
-			position = "left",
-			size = 40,
-		},
-		{
-			elements = {
-				{
-					id = "repl",
-					size = 0.5,
-				},
-				{
-					id = "console",
-					size = 0.5,
-				},
-			},
-			position = "bottom",
-			size = 10,
-		},
-	},
-	mappings = {
-		edit = "e",
-		expand = { "<CR>", "<2-LeftMouse>" },
-		open = "o",
-		remove = "d",
-		repl = "r",
-		toggle = "t",
-	},
-	render = {
-		indent = 1,
-		max_value_lines = 100,
-	},
+    controls = {
+        element = "repl",
+        enabled = true,
+        icons = {
+            disconnect = "",
+            pause = "",
+            play = "",
+            run_last = "",
+            step_back = "",
+            step_into = "",
+            step_out = "",
+            step_over = "",
+            terminate = "",
+        },
+    },
+    element_mappings = {},
+    expand_lines = true,
+    floating = {
+        border = "single",
+        mappings = {
+            close = { "q", "<Esc>" },
+        },
+    },
+    force_buffers = true,
+    icons = {
+        collapsed = "",
+        current_frame = "",
+        expanded = "",
+    },
+    layouts = {
+        {
+            elements = {
+                {
+                    id = "scopes",
+                    size = 0.25,
+                },
+                {
+                    id = "breakpoints",
+                    size = 0.25,
+                },
+                {
+                    id = "stacks",
+                    size = 0.25,
+                },
+                {
+                    id = "watches",
+                    size = 0.25,
+                },
+            },
+            position = "left",
+            size = 40,
+        },
+        {
+            elements = {
+                {
+                    id = "repl",
+                    size = 0.5,
+                },
+                {
+                    id = "console",
+                    size = 0.5,
+                },
+            },
+            position = "bottom",
+            size = 10,
+        },
+    },
+    mappings = {
+        edit = "e",
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        repl = "r",
+        toggle = "t",
+    },
+    render = {
+        indent = 1,
+        max_value_lines = 100,
+    },
 }
 
 function m.setup()
-	local dap, dapui = require("dap"), require("dapui")
+    local dap, dapui = require("dap"), require("dapui")
 
-	-- Setup auto open/close for dap-ui
-	dap.listeners.after.event_initialized["dapui_config"] = function()
-		dapui.open()
-	end
-	dap.listeners.before.event_terminated["dapui_config"] = function()
-		dapui.close()
-	end
-	dap.listeners.before.event_exited["dapui_config"] = function()
-		dapui.close()
-	end
+    -- Setup auto open/close for dap-ui
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+    end
 
-	dapui.setup(ui_cfg)
+    dapui.setup(ui_cfg)
+    require("neodev").setup({
+        library = { plugins = { "nvim-dap-ui" }, types = true },
+    })
 
-	require("neodev").setup({
-		library = { plugins = { "nvim-dap-ui" }, types = true },
-	})
-
-	require("plugins.debug.go").setup(dap)
+    require("plugins.debug.go").setup(dap)
 end
 
 return m
--- plugins/init.lua
+++ plugins/init.lua
@@ -3,86 +3,118 @@ local packer = require("packer")
 
 -- Open packer in a floating window
 packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+    display = {
+        open_fn = function()
+            return require("packer.util").float({ border = "rounded" })
+        end,
+    },
 })
 
 packer.startup(function(use)
-	-- Packer package manager
-	use("wbthomason/packer.nvim")
+    -- Packer package manager
+    use("wbthomason/packer.nvim")
 
-	-- Lspconfig
-	require("plugins.lsp.init").setup(use)
-	require("plugins.autocompletion.init").setup(use)
+    -- Lspconfig
+    require("plugins.lsp.init").setup(use)
+    require("plugins.autocompletion.init").setup(use)
 
-	--  UI
-	use({
-		require("plugins.theme").setup("onedarkWarm"),
-		require("plugins.feline"),
-		require("plugins.nvim_tree"),
-		require("plugins.indentline"),
-		{
-			"nvim-telescope/telescope.nvim",
-			requires = { "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim" },
-		},
-		{
-			"glepnir/dashboard-nvim",
-			event = "VimEnter",
-			config = function()
-				require("dashboard").setup({
-					-- config
-				})
-			end,
-		},
-		{
-			"akinsho/bufferline.nvim",
-			requires = "kyazdani42/nvim-web-devicons",
-			config = function()
-				require("plugins/bufferline")
-			end,
-		},
-		{
-			"folke/which-key.nvim",
-			config = function()
-				require("plugins/which_key")
-			end,
-		},
-		{
-			"numToStr/Comment.nvim",
-			config = function()
-				require("Comment").setup()
-			end,
-		},
-		"voldikss/vim-floaterm",
-		{
-			"lewis6991/gitsigns.nvim",
-			requires = "nvim-lua/plenary.nvim",
-			config = function()
-				require("gitsigns").setup()
-			end,
-		},
-	})
+    --  UI
+    use({
+        require("plugins.theme").setup("onedarkWarm"),
+        require("plugins.feline"),
+        require("plugins.nvim_tree"),
+        require("plugins.indentline"),
+        {
+            "nvim-telescope/telescope.nvim",
+            requires = { "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim" },
+        },
+        {
+            "glepnir/dashboard-nvim",
+            event = "VimEnter",
+            config = function()
+                require("dashboard").setup({
+                    -- config
+                })
+            end,
+        },
+        {
+            "akinsho/bufferline.nvim",
+            requires = "kyazdani42/nvim-web-devicons",
+            config = function()
+                require("plugins/bufferline")
+            end,
+        },
+        {
+            "folke/which-key.nvim",
+            config = function()
+                require("plugins/which_key")
+            end,
+        },
+        {
+            "numToStr/Comment.nvim",
+            config = function()
+                require("Comment").setup()
+            end,
+        },
+        "voldikss/vim-floaterm",
+        {
+            "lewis6991/gitsigns.nvim",
+            requires = "nvim-lua/plenary.nvim",
+            config = function()
+                require("gitsigns").setup()
+            end,
+        },
+    })
 
-	-- Golang
-	use({
-		{
-			"ray-x/go.nvim",
-			config = function()
-				require("go").setup()
-			end,
-		},
-		"ray-x/guihua.lua",
-	})
+    -- Golang
+    use({
+        "ray-x/go.nvim",
+        config = function()
+            require("go").setup()
+        end,
+        "ray-x/guihua.lua",
+    })
 
-	-- Debug
-	use({
-		"rcarriga/nvim-dap-ui",
-		requires = { "mfussenegger/nvim-dap", "folke/neodev.nvim" },
-		config = function()
-			require("plugins.debug.init").setup()
-		end,
-	})
+    -- Testing
+    use({
+        "nvim-neotest/neotest",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-neotest/neotest-go",
+        },
+        config = function()
+            -- get neotest namespace (api call creates or returns namespace)
+            local neotest_ns = vim.api.nvim_create_namespace("neotest")
+            vim.diagnostic.config({
+                virtual_text = {
+                    format = function(diagnostic)
+                        local message =
+                            diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+                        return message
+                    end,
+                },
+            }, neotest_ns)
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-go")({
+                        experimental = {
+                            test_table = true,
+                        },
+                        args = { "-count=1", "-timeout=60s" },
+                    }),
+                },
+            })
+        end,
+    })
+
+    -- Debug
+    use({
+        "rcarriga/nvim-dap-ui",
+        requires = { "mfussenegger/nvim-dap", "folke/neodev.nvim" },
+        config = function()
+            require("plugins.debug.init").setup()
+        end,
+    })
 end)
