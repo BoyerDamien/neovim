3ad7f60ecb68e390892feecbd7644c5b23c84167
BOYER_Damien <damien.boyer@astrachain.com>
2023-04-12 17:34:24 +0200

    fix: indentline on dashboard

--- plugins/indentline.lua
+++ plugins/indentline.lua
@@ -1,12 +1,14 @@
 return {
-    'lukas-reineke/indent-blankline.nvim',
-    branch = 'master',
+    "lukas-reineke/indent-blankline.nvim",
+    branch = "master",
     config = function()
-        require("indent_blankline").setup {
+        require("indent_blankline").setup({
             show_current_context = true,
+            show_current_context_start = true,
             buftype_exclude = { "terminal" },
             use_treesitter = true,
-            bufname_exclude = { "dashboard" },
-        }
-    end
+            bufname_exclude = { "dashboard", "dashboard-1" },
+            show_end_of_line = true,
+        })
+    end,
 }
