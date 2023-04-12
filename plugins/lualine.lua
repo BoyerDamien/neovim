07d5784969f8ef82ab6bd14729338f55abcbc1f2
BOYER_Damien <damien.boyer@astrachain.com>
2023-04-12 17:46:27 +0200

    fix: remove feline

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -1,7 +1,9 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    config = function()
-        require('lualine').setup()
-    end
+	"nvim-lualine/lualine.nvim",
+	requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	config = function()
+		require("lualine").setup({
+			theme = "onedark",
+		})
+	end,
 }
