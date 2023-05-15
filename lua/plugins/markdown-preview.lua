c7197dba76ec6129af4c3e52c3a8820d01813b65
BOYER_Damien <damien@astran.io>
2023-05-15 15:50:48 +0200

    fix: markdown preview configuration

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,8 +1,4 @@
 return {
   "iamcco/markdown-preview.nvim",
-  run = "cd app && npm install",
-  setup = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
+  config = function() vim.fn["mkdp#util#install"]() end,
 }
