2ad6a8fb59984c36f489d439110d9e5d2fba43b7
Damien Boyer <d.boyer@go-electra.com>
2025-06-12 18:55:36 +0200

    fix: add codecompanion instead of avante

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.nix" },
 
     -- Linting
     { import = "lazyvim.plugins.extras.linting.eslint" },
